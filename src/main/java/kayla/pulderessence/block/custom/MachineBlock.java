a63e4a0ef984c5210d6b629a9e71304764738eaf
KaylaDoesStuff <kayla.trevena982@gmail.com>
2025-02-11 18:44:11 +1300

    creating a MachineBlock class for the base machine BlockEntity

---
+++ src/main/java/kayla/pulderessence/block/custom/MachineBlock.java
@@ -0,0 +1,42 @@
+package kayla.pulderessence.block.custom;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.BlockRenderView;
+import net.minecraft.world.World;
+import net.minecraft.world.event.listener.GameEventListener;
+import org.jetbrains.annotations.Nullable;
+
+public class MachineBlock extends BlockWithEntity {
+    public MachineBlock(Settings settings) {
+        super(settings);
+    }
+
+
+    @Override
+    public @Nullable BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return null;
+    }
+
+    @Override
+    public @Nullable <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
+        return super.getTicker(world, state, type);
+    }
+
+    @Override
+    public @Nullable <T extends BlockEntity> GameEventListener getGameEventListener(ServerWorld world, T blockEntity) {
+        return super.getGameEventListener(world, blockEntity);
+    }
+
+    @Override
+    public BlockState getAppearance(BlockState state, BlockRenderView renderView, BlockPos pos, Direction side, @Nullable BlockState sourceState, @Nullable BlockPos sourcePos) {
+        return super.getAppearance(state, renderView, pos, side, sourceState, sourcePos);
+    }
+}

f2763559be9898f9e598f4a0fef2b1fde4fb424b
KaylaDoesStuff <kayla.trevena982@gmail.com>
2025-02-14 17:27:01 +1300

    fixed trapdoor/door, added textures.

--- src/client/java/kayla/pulderessence/PulderEssenceClient.java
+++ src/client/java/kayla/pulderessence/PulderEssenceClient.java
@@ -1,10 +1,13 @@
 package kayla.pulderessence;
 
+import kayla.pulderessence.block.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 
 public class PulderEssenceClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PULVERENT_TRAPDOOR, RenderLayer.getCutout());
 	}
 }

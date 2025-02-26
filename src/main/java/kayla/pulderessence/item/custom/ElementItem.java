ddce5240bf049a7dc84c4f756de637c3f0fa2166
KaylaDoesStuff <kayla.trevena982@gmail.com>
2025-02-26 21:54:11 +1300

    added mass/number elements

--- src/main/java/kayla/pulderessence/item/custom/ElementItem.java
+++ src/main/java/kayla/pulderessence/item/custom/ElementItem.java
@@ -12,10 +12,14 @@ import java.util.List;
 
 public class ElementItem extends Item {
     private final String elementID;
+    private final float atomicMass;
+    private final int atomicNumber;
 
-    public ElementItem(Settings settings, String element) {
+    public ElementItem(Settings settings, String element, int number, float mass) {
         super(settings);
         this.elementID = element;
+        this.atomicMass = mass;
+        this.atomicNumber = number;
     }
     @Override
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
